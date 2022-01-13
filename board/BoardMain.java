72a8dfffcac5d9852af310dc77d4cecd0174169d
donhyuck <leedonhyuck123@gmail.com>
2022-01-13 16:27:51 +0900

    JDBC,mySQL연동 구현중

--- board/BoardMain.java
+++ board/BoardMain.java
@@ -1,7 +1,9 @@
 package board;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class BoardMain {
@@ -11,9 +13,6 @@ public class BoardMain {
 
 		// 명령어를 통해 Array에 정보가 저장되어도 main함수가 끝나면 소멸된다.
 		// 다시 시작하면 이전 데이터가 사라진다. => DB가 필요한 이유
-		List<Article> articles = new ArrayList<>();
-
-		int lastArticleId = 1;
 
 		System.out.println("== 프로그램 시작 ==");
 
@@ -35,7 +34,6 @@ public class BoardMain {
 			} else if (command.equals("article write")) {
 				System.out.println("== 게시글 작성 ==");
 
-				int id = lastArticleId;
 				String title;
 				String body;
 
@@ -45,26 +43,52 @@ public class BoardMain {
 				System.out.print("내용 : ");
 				body = input.nextLine();
 
-				Article article = new Article(id, title, body);
-				articles.add(article);
-				System.out.printf("%d번 글이 등록되었습니다.\n", id);
+				// JDBC적용
+				Connection conn = null; // DB 접속 객체
+				PreparedStatement pstat = null; // SQL 구문을 실행하는 역할
 
-				lastArticleId++;
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver"); // Mysql JDBC 드라이버 로딩
+					String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 
-			}
+					conn = DriverManager.getConnection(url, "root", "");
 
-			else if (command.equals("article list")) {
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = \"" + title + "\"";
+					sql += ", body = \"" + body + "\"";
 
-				if (articles.size() == 0) {
-					System.out.println("게시글이 존재하지 않습니다.");
+					pstat = conn.prepareStatement(sql);
+					int affectedRows = pstat.executeUpdate();
+
+					System.out.println("affectedRows: " + affectedRows);
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally { // 예외 상황이든 아니든 무조건 마지막에 실행하는 finally
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close(); // 연결 종료
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+					try {
+						if (pstat != null && !pstat.isClosed()) {
+							pstat.close(); // 연결 종료
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
 				}
 
-				System.out.println("== 게시글 목록 ==");
-				System.out.println("번호 / 제목 ");
+			} else if (command.equals("article list")) {
 
-				for (Article article : articles) {
-					System.out.printf(" %d / %s \n", article.id, article.title);
-				}
+				// 재구성 예정
 
 			} else {
 				System.out.println("잘못된 명령어입니다.");
